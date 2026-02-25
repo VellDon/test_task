06252656b08a12e839b9a73fcfe2db1c1338d451
annettta <kovshovi444@gmail.com>
2026-02-26 01:09:26 +0300

    mult

---
+++ test_task1488/рабочий_гит/testing_pp/testing/multi_stream.cpp
@@ -0,0 +1,102 @@
+#include <vector>
+#include <iostream>
+#include <spdlog/spdlog.h>
+//#include <boost/program_options.hpp>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <cstdint>   // для int64_t
+
+int mult_stream(std::string &name_file){
+  int count_file = 0;
+  std::ifstream name(name_file);
+  if(!name.is_open()){
+    spdlog::error("Не удалось открыть файл");
+    return 1;
+  }
+  std::string line;
+  std::getline(name, line);
+  std::stringstream ss;
+  std::string token;
+  int count = 0;
+  int count_rts = 0;
+  int count_price = 0;
+  while (std::getline(ss, token, ';'))
+  {
+    count++;
+    if (token == "receive_ts")
+    {
+        count_rts = count;
+    }
+    if (token == "price")
+    {
+        count_price = count;
+    }
+  }
+  int count_line = 0;
+  while(std::getline(name, line))
+  {
+    count_line++;
+    if(line.empty())
+    {
+        continue;
+    }
+    std::stringstream sss(line);
+    std::vector<std::pair<int64_t, double>> data;
+    int64_t time = 0;
+    double price = 0.0;
+    bool valid_row = true;
+    count = 0;
+
+    while(std::getline(sss, token, ';'))
+    {
+        count++;
+        try{
+            if(count == count_price){
+                price = std::stod(token);
+            }
+            if(count == count_rts){
+                time = std::stoll(token);
+            }
+
+        }catch(const std::exception &e){
+            spdlog::warn("Ошибка преобразования в файле {}: {} (строка: {})", name_file, e.what(), line);
+            valid_row = false;
+            break;
+
+        }
+    }
+    if(valid_row){
+        data.emplace_back(time, price);
+    }
+    if(count_line > 2){
+        count_file++;
+        std::stringstream temp;
+        temp << "temp_file_" << count_file << ".txt";
+        std::string temp_name = temp.str();
+
+        std::sort(data.begin(), data.end(), [](auto &a, auto &b){
+            return a.first < b.first;
+        });
+        std::ofstream file(temp_name);
+        if(!file.is_open()){
+            spdlog::error("ошибка создания файла");
+            return 1;
+        }
+        for(auto &num : data){
+            file << num.first << ";" << num.second;
+        }
+
+    }
+
+
+  }
+
+
+  return 0;
+
+}
+int main(){
+    std::string s = "Trade.cvs";
+    mult_stream(s);
+}
